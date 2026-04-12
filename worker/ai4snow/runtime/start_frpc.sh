e54521e7511a3ec16920b5d70e40aadbed2d1303
13384996200 <947441390@qq.com>
2026-04-12 13:29:49 +0800

    chore: 移除 worker frp 运行时文件

--- worker/ai4snow/runtime/start_frpc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec "/Users/murphy/Library/Mobile Documents/com~apple~CloudDocs/Projects/滑雪项目/Ski_Avatar_Pro/runtime/frp_darwin_arm64/frpc" -c "/Users/murphy/Library/Mobile Documents/com~apple~CloudDocs/Projects/滑雪项目/Ski_Avatar_Pro/runtime/frp_darwin_arm64/frpc.toml" >> "/Users/murphy/Library/Mobile Documents/com~apple~CloudDocs/Projects/滑雪项目/Ski_Avatar_Pro/runtime/frp_darwin_arm64/frpc.log" 2>&1
